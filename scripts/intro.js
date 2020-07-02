ed56fd94f331ea1dd15f3a97fc462dac59b3ac63
Jair Raya <jairrayas@outlook.com>
2020-07-02 02:51:01 -0300

    introducao a java script

---
+++ scripts/intro.js
@@ -0,0 +1,68 @@
+// coments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let myvar = "He";
+
+// const myconst = "He";
+
+// document.write(myconst + myvar);
+
+// String
+// const s1 = "String"
+// const s2 = 'String'
+// const s3 = `String`
+
+// Number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// booblean - true or false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objetos possuem propriedades e funcionalidades
+
+// const pessoa = {
+//   altura: "1,80m",
+//   idade:  24,
+//   solteiro: true,
+//   correr(){
+//     document.write("Executar uma grande lógica de correr.")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array -  Vetores
+// const colecaoDeBolinhas = ["blue","green", 1, {name: "Jair"}]
+
+// document.write(colecaoDeBolinhas[0 + 1 + 1 ])
+
+// Funcoes - Funcionalidades - Atalhos - Reuso de codigo
+
+// registrada
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Jair")
+
+// condicionais
+// const notaFinal = 5
+
+// if(notaFinal < 5){
+//   document.write("Reprovado")
+// } else{
+//   document.write("Aprovado")
+// }
+  
+
+// Loop - Repeticoes
+
+// for(i = 0; i < 10; i++){
+//   document.write(`<p> ${i} </p>`)
+// }
