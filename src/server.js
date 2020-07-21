83b6f50c6da076652e3d195700456e1cd0a08391
Jair Raya <jairrayas@outlook.com>
2020-07-21 03:49:01 -0300

    instalando nunjucks

--- src/server.js
+++ src/server.js
@@ -1,14 +1,31 @@
 const express = require("express")
 const server = express()
 
+// configurar pasta publica
+server.use(express.static("public"))
+
+// utilizando tempate engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 // configurar caminhos da minha aplicacao
 // pagina inicial
 // req: Requisicao
 // res: Respostas
 server.get("/", (req, res) => {
-    res.send("Hello world!")
+    return res.render("index.html", {title: " Um título"})
 })
 
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
 
 // ligar o servidor
 server.listen(3000)
