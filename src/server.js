c8793359352ae07e1dfab85997fb0ef31e36caef
Jair Raya <jairrayas@outlook.com>
2020-07-20 23:44:44 -0300

    nodemon funcionou!

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 const express = require("express")
 const server = express()
 
+// configurar caminhos da minha aplicacao
+// pagina inicial
+// req: Requisicao
+// res: Respostas
+server.get("/", (req, res) => {
+    res.send("Hello world!")
+})
+
+
 // ligar o servidor
 server.listen(3000)
