b5d9ee2b11e99f3f869ffa9442454be9ada2e95f
Jair Raya <jairrayas@outlook.com>
2020-07-26 01:58:18 -0300

    pegando os dados do banco de dados e mostrando no html

--- src/database/db.js
+++ src/database/db.js
@@ -4,43 +4,84 @@ const sqlite3 = require("sqlite3").verbose()
 // criar o objeto que ira fazer operacoes no banco de dados
 const db = new sqlite3.Database("./src/database/database.db")
 
+module.exports = db
+
 // utilizar o objeto de banco de dados, para nossa operacoes
-db.serialize(() =>{
+// db.serialize(() =>{
     // com comandos SQL eu vou:
 
     // 1. Criar uma tabela
 
-    db.run(`
-        create table if not exists places(
-            id integer primary key autoincrement,
-            name text,
-            image text,
-            address text,
-            address2 text,
-            state text,
-            city text,
-            items text
-        );
-    `)
+    // db.run(`
+    //     create table if not exists places(
+    //         id integer primary key autoincrement,
+    //         image text,
+    //         name text,
+    //         address text,
+    //         address2 text,
+    //         state text,
+    //         city text,
+    //         items text
+    //     );
+    // `)
 
     // 2. Inserir dados na tabela
-    const query = `
-        insert into places(
-            name,
-            image,
-            address,
-            address2,
-            state,
-            city,
-            items
-        ) values(?,?,?,?,?,?,?);
-    `
-    db.run(query, [])
+    // const query = `
+    //     insert into places(
+    //         image,
+    //         name,
+    //         address,
+    //         address2,
+    //         state,
+    //         city,
+    //         items
+    //     ) values(?,?,?,?,?,?,?);
+    // `
 
+    // const values = [
+    // "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2901&q=80",
+    // "Colectoria",
+    // "Rua Guilherme Gemballa, Jardim América",
+    // "N°: 260",
+    // "Santa Catarina",
+    // "Rio do Sul",
+    // "Resíduos Eletrônicos, Lâmpadas"
+    // ]
 
+    // const values = [
+    //     "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    //     "Papersider",
+    //     "Rua Guilherme Gemballa, Jardim América",
+    //     "N°: 260",
+    //     "Santa Catarina",
+    //     "Rio do Sul",
+    //     "Papéis e Papelão"
+    //     ]
 
+    // function afterInsertData(err) {
+    //     if(err){
+    //         return console.log(err)
+    //     }
+    //     console.log("Cadastrado com sucesso!")
+    //     console.log(this)
+    // }
+
+    // db.run(query, values, afterInsertData)
 
     // 3. Consultar dados na tabela
+    // db.all(`select * from places`, function(err, rows) {
+    //     if(err){
+    //         return console.log(err)
+    //     }
+    //     console.log("Aqui estão seu registros: ")
+    //     console.log(rows)
+    // })
 
     // 4. Deletar um dado na tabela
-})
+    // db.run(`delete from places where id = ?`, [1], function(err) {
+    //     if(err){
+    //         return console.log(err)
+    //     }
+    //     console.log("Registro deletado com sucesso!")
+    // })
+// })
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const server = express()
 
+// pegar o banco de dados
+const db = require("./database/db")
+
 // configurar pasta publica
 server.use(express.static("public"))
 
@@ -24,7 +27,21 @@ server.get("/create-point", (req, res) => {
 })
 
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    // pegar os dados do bando de dados
+    db.all(`select * from places`, function(err, rows) {
+        if(err){
+            return console.log(err)
+        }
+
+        // console.log("Aqui estão seu registros: ")
+        // console.log(rows)
+
+        // mostrar a pagina html com os dados do banco de dados
+        return res.render("search-results.html", {places: rows})
+
+    })
+
 })
 
 // ligar o servidor
